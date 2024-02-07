e1126342bfbe4692d15ee107436dc59dcf4435fa
Tobit Flatscher <53856473+2b-t@users.noreply.github.com>
2024-02-07 23:30:49 +0000

    refactor: Simplify CMake files

--- ConfigExtras.cmake
+++
@@ -1,6 +0,0 @@
-# See https://answers.ros.org/question/331089/ament_export_dependenciesboost-not-working/?answer=332460#post-id-332460
-
-if(PYTHON_BINDINGS)
-  find_package(Python REQUIRED COMPONENTS Interpreter Development)
-  find_package(pybind11 CONFIG REQUIRED)
-endif()
