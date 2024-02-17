9c78d3a873ea05ee069f102b87a531df9fb6b8e6
Tobit Flatscher <53856473+2b-t@users.noreply.github.com>
2024-02-17 00:59:19 +0000

    feat: Import all submodules in __init__.py

--- myactuator_rmd/__init__.py
+++ myactuator_rmd/__init__.py
@@ -1,1 +1,15 @@
-from myactuator_rmd import myactuator_rmd_py
+"""
+Author: Tobit Flatscher (github.com/2b-t)
+
+Imports all submodules
+Taken from https://stackoverflow.com/a/3365846
+"""
+
+import pkgutil
+
+
+__all__ = []
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    _module = loader.find_module(module_name).load_module(module_name)
+    globals()[module_name] = _module
