6504b1785e3d8b1905b62cff9057193ae2924c94
Tobit Flatscher <53856473+2b-t@users.noreply.github.com>
2024-02-18 22:26:14 +0000

    fix: Revert commit 9c78d3a873ea05ee069f102b87a531df9fb6b8e6 as results in import error

--- myactuator_rmd/__init__.py
+++ myactuator_rmd/__init__.py
@@ -6,11 +6,4 @@
    Tobit Flatscher (github.com/2b-t)
 """
 
-import pkgutil
-
-
-__all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+from myactuator_rmd import myactuator_rmd_py
